77041ccc148b2e4f3e0f4027874fb6a2018f0638
Blocktomo <thomaspublicdata@gmail.com>
2024-03-02 16:57:27 +0100

    etat initial

---
+++ src/main/java/fr/insa/beverly/testproject/Hello.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package fr.insa.beverly.testproject;
+
+/**
+ *
+ * @author thomas beverly
+ */
+public class Hello {
+    
+    public static void main(String []args){
+        System.out.println("Heeeellllllo!! test 1.");
+        
+    }
+    
+}
