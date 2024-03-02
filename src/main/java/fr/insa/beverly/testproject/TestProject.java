47c959b82fea949ed20365ac00795d5a56d3ce03
Blocktomo <thomaspublicdata@gmail.com>
2024-03-02 18:21:29 +0100

    modif de aujourd'hui

---
+++ src/main/java/fr/insa/beverly/testproject/TestProject.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package fr.insa.beverly.testproject;
+
+/**
+ *
+ * @author thomas beverly
+ */
+public class TestProject {
+    public static int calcul(int a, int b){
+        return a*b;
+    }
+    public static void main(String[] args) {
+        System.out.println("Hello World! Kartoffel!");
+        System.out.println(calcul(2,3)); 
+    }
+}
